6cf4de60f84e70e224b28ad8e309fdb5a44df9d5
Robert Fuchs <robert.fuchs@logmein.com>
2021-06-12 22:24:36 +0200

    fetch mails and upload them to S3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 import { fetchMails } from "./gmail";
+import { uploadToS3 } from "./s3";
+import { createHash } from 'crypto';
 
-const mails = fetchMails();
+interface FileContent {
+  title: string;
+  content: string;
+  date?: string;
+  preview?: string;
+}
+
+async function convertMailToFile() {
+  const mails = await fetchMails();
+  const files: FileContent[] = mails.filter(mail => (mail.subject && mail.body.text)).map(mail => (
+    {
+      title: mail.subject!,
+      content: mail.body.text!,
+      date: mail.receivedOn,
+      preview: mail.snippet,
+    }
+  ));
+
+  
+  files.forEach(file => {
+    const name = createHash('md5').update(file.title).digest('hex');
+    uploadToS3(name, JSON.stringify(file));
+  });
+}
+
+convertMailToFile();
+
